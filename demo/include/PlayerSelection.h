c745cc9736d0a4cddebc3c48fb817817e274af91
Lebedev Egor <egorikl@list.ru>
2020-05-17 16:54:48 +0300

    Fix very good code from PlayerSelection

--- demo/include/PlayerSelection.h
+++ demo/include/PlayerSelection.h
@@ -15,40 +15,22 @@
 class PlayerSelection : public QObject {
 Q_OBJECT
 public:
-
     PlayerSelection(Scene *scene);
-
     ~PlayerSelection() override;
 
     void add_players(const std::vector<Player *> &players);
-
     void run_player_selection();
-
     void clear_player_selection();
-
     void init_window();
-
     void set_text();
-
     void set_buttons();
-
     void set_images();
-
-    void set_buttons_player1();
-
-    void set_buttons_player2();
-
-    void set_buttons_player3();
-
-    void set_buttons_player4();
+    void set_buttons_player(int index, int xPos);
 
 
 public slots:
-
     void change_image(int player_number);
-
     void increase_ready_num(int player_number);
-
     void decrease_ready_num(int player_number);
 
 signals:
@@ -62,22 +44,10 @@ private:
     QLabel *character_selection;
     std::vector<QLabel *> player_num;
 
-    std::vector<QPushButton *> buttons_player1;
-    std::vector<QPushButton *> buttons_player2;
-    std::vector<QPushButton *> buttons_player3;
-    std::vector<QPushButton *> buttons_player4;
-    QPushButton *customize_player1;
-    QPushButton *customize_player2;
-    QPushButton *customize_player3;
-    QPushButton *customize_player4;
-    QPushButton *ready_player1;
-    QPushButton *ready_player2;
-    QPushButton *ready_player3;
-    QPushButton *ready_player4;
-    QPushButton *back_player1;
-    QPushButton *back_player2;
-    QPushButton *back_player3;
-    QPushButton *back_player4;
+    std::vector<QPushButton *> buttons_player[4];
+    QPushButton *customize_player[4];
+    QPushButton *ready_player[4];
+    QPushButton *back_player[4];
 
     std::vector<QString> available_skins{"images/demo_player.png", "images/demo_player_2.png"};
     std::vector<QGraphicsPixmapItem *> player_textures;
